ce1578686759ddc843ed71b2d105d79c9f0851cd
Nipun Chamika Eheliyagoda <nipunchamika1818@gmail.com>
2025-12-24 20:04:06 +0530

    Fix formatting in App.jsx
    changed

--- medi-clock/src/App.jsx
+++ medi-clock/src/App.jsx
@@ -24,7 +24,7 @@ import SubmitSuccess from "./componants/SubmitSuccess";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+     <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signup2" element={<Signup2 />} />
       <Route path="/signup3" element={<Signup3 />} />
