bd1db67365a34dbec5f43371a200b27ed8c22074
Gayantha Hashan <wijehashn@gmail.com>
2025-12-24 17:24:12 +0530

    Signup2 input update
    Sign Up 2 page Input was updated to more advanced path.

--- medi-clock/src/componants/Signup2.jsx
+++ medi-clock/src/componants/Signup2.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import './css/Signup.css'
 import AppLogo from "./assets/logo.svg"
 import logo2 from "./assets/lower-art.svg"
 
 const Signup2 = () => {
+  const [mobile, setMobile] = useState('');
+
   const navigate = useNavigate();
+
+  const handleRequestOTP = () => {
+  const phone = mobile.trim();
+
+  const phoneRegex = /^[0-9]{10}$/;
+  if (!phoneRegex.test(phone)) {
+    alert("Invalid Phone Number");
+    return;
+  }
+
+  navigate("/Signup3");
+};
+
   return (
        <div className='Signup'>
               <img src={AppLogo} alt="logo" />
@@ -16,9 +31,15 @@ const Signup2 = () => {
             </div>
         <div className='userNo'>
             Mobile Number<br></br>
-            <input type="text" placeholder='Phone Numeber Here' className='Sinput'></input><br></br>
+            <input
+              type="text"
+              placeholder='Phone Number Here'
+              className='Sinput'
+              value={mobile}
+              onChange={(e) => setMobile(e.target.value)} /><br></br>
+
         </div>
-        <div className='signupbutton'><button className='button' onClick={() => navigate("/Signup3")}><span>REQUEST OTP</span></button></div>
+        <div className='signupbutton'><button type="button" className='button' onClick={handleRequestOTP}><span>REQUEST OTP</span></button></div>
         <img src={logo2} alt="logo2" className='logo2'/>
         </div>
     
